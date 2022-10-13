4dcb71ad05e94b046dc0d0088487ab9b3b7ebbda
Kirill Morozov <morozov.ks@gmail.com>
2022-10-13 15:09:26 +0300

    refact: all logic refact #2

---
+++ FakeSlackAPI.js
@@ -0,0 +1,77 @@
+import { faker } from 'https://cdn.skypack.dev/@faker-js/faker';
+import { messages, replyMessage1, replyMessage2 } from './slackData.js';
+
+export class FakeSlackAPI {
+  constructor(config) {
+    this._url = config.url;
+    this._token = config.token;
+    this.conversation = {
+      channel: null,
+      oldest: null,
+      latest: null,
+    };
+  }
+  // устанавливаем диапазон сообщений с которыми мы будем работать
+  // под капотом "парсим" ссылки, разбивая их на channel, oldest, latest и получится:
+  //   channel=C03L9DBH708
+  //   oldest=1660057772.213509
+  //   latest=1660142789.914989
+
+  setConversationRange({ firstMessageURL, lastMessageURL }) {
+    const firstURL = new URL(firstMessageURL);
+    const lastURL = new URL(lastMessageURL);
+
+    const [, , channel, oldestString] = firstURL.pathname.split('/');
+    const [, , , latestString] = lastURL.pathname.split('/');
+    // Получаем регуляркой все числа в конце строки. Может вернуться или массив или null, поэтому есть ИЛИ.
+    const [parsedOldestString] = oldestString.match(/\d+$/) || [''];
+    const [parsedLatestString] = latestString.match(/\d+$/) || [''];
+    // Превращаем строки в числа (моет превратиться в NaN)
+    const oldestNs = parseFloat(parsedOldestString);
+    const latestNs = parseFloat(parsedLatestString);
+    const nanoSecondsInMilliseconds = 1000000;
+
+    if (isNaN(oldestNs) || isNaN(latestNs) || !channel) {
+      // Пусть кто-то снаружи узнает, что что-то не так. Можно возвращать объект ошибки
+      return false;
+    }
+
+    const oldest = oldestNs / nanoSecondsInMilliseconds;
+    const latest = oldestNs / nanoSecondsInMilliseconds;
+
+    this.conversation = {
+      channel,
+      oldest,
+      latest,
+    };
+
+    return true;
+  }
+
+  // GET /conversations.history получаем все сообщения
+  // Обрати внимание на флаг has_more и подумай о рекурсии
+  async getAllConversations() {
+    return messages.messages;
+  }
+
+  // GET /conversations.replies получаем все треды
+  // conversationTS - значение поля "ts" из массива "messages", который пришёл из getAllConversations()
+  async getAllReplies(conversationTS) {
+    if (conversationTS === replyMessage1.messages[0].thread_ts) {
+      return replyMessage1.messages;
+    }
+    return replyMessage2.messages;
+  }
+
+  // GET /users.profile.get получаем пользователя
+  // userID - значение поля "user" из массива "messages", который пришёл из getAllConversations() или getAllReplies
+  async getUser(userID) {
+    const user = {
+      user: userID,
+      display_name: faker.name.fullName(),
+      image_original: faker.image.avatar(),
+    };
+
+    return user;
+  }
+}
--- index.js
+++ index.js
@@ -1,213 +1,85 @@
-import { messages, replyMessage1, replyMessage2 } from "./slackData.js";
-import { faker } from 'https://cdn.skypack.dev/@faker-js/faker';
+import { FakeSlackAPI } from './FakeSlackAPI.js';
 
-const config =
-  {
-    url: 'https://slack.com/', // куда стучаться за данными
-    token: 'some-slack-token', // с каким токеном стучаться
-    historyURL: 'api/conversations.history', // pathname для запроса всех сообщений
-    repliesURL: 'conversations.replies', // pathname для запроса реплаев
-    userURL: 'users.profile.get' // pathname для запроса юзеров
-  }
+const config = {
+  url: 'https://slack.com/api/', // куда стучаться за данными
+  token: 'some-slack-token', // с каким токеном стучаться
+};
 
 const objURL = {
   firstMessageURL: 'https://hexlet-students.slack.com/archives/C03L9DBH708/p1660057772213509',
   lastMessageURL: 'https://hexlet-students.slack.com/archives/C03L9DBH708/p1660142789914989',
-}
-
-class FakeSlackAPI {
-  constructor(config) {
-    this._url = config.url;
-    this._token = config.token;
-    this._historyURL = config.historyURL;
-    this._repliesURL = config.repliesURL;
-    this._userURL = config.userURL;
-  }
-
-  // устанавливаем диапазон сообщений с которыми мы будем работать
-  // под капотом "парсим" ссылки, разбивая их на channel, oldest, latest и получится:
-//   channel=C03L9DBH708
-//   oldest=1660057772.213509
-//   latest=1660142789.914989
-  setConversationRange(data) {
-
-    const firstURL = new URL(data.firstMessageURL);
-    const lastURL = new URL(data.lastMessageURL);
-
-    const [, , channel, oldestString] = firstURL.pathname.split('/');
-    const [, , , latestString] = lastURL.pathname.split('/');
-
-    const nanoSecondsInMilliseconds = 1000000;
-
-    this._range = {};
-    this._range.channel = channel;
-    this._range.oldest = oldestString.replace(/^p/,'') / nanoSecondsInMilliseconds;
-    this._range.latest = latestString.replace(/^p/,'') / nanoSecondsInMilliseconds;
-  }
-
-// GET /conversations.history получаем все сообщения
-// Обрати внимание на флаг has_more и подумай о рекурсии
-  getAllConversations() {
-    const historyURL = new URL(`${this._url}`);
-    historyURL.pathname = this._historyURL;
-    historyURL.searchParams.set('channel', `${this._range.channel}`);
-    historyURL.searchParams.set('inclusive', `true`);
-    historyURL.searchParams.set('latest', `${this._range.latest}`);
-    historyURL.searchParams.set('limit', '5');
-    historyURL.searchParams.set('oldest', `${this._range.oldest}`);
-
-    return messages.messages;
-  }
-
-// GET /conversations.replies получаем все треды
-// conversationTS - значение поля "ts" из массива "messages", который пришёл из getAllConversations()
-
-  getAllReplies(conversationTS) {
-    const repliesURL = new URL(`${this._url}`);
-    repliesURL.pathname = this._repliesURL;
-    repliesURL.searchParams.set('channel', `${this._range.channel}`);
-    repliesURL.searchParams.set('ts', `${conversationTS}`);
-
-    if (conversationTS === replyMessage1.messages[0].thread_ts) {
-      return replyMessage1.messages
-    } else {
-      return replyMessage2.messages;
-    }
-  }
-
-// GET /users.profile.get получаем пользователя
-// userID - значение поля "user" из массива "messages", который пришёл из getAllConversations() или getAllReplies
-
-  getAllUsers(userID) {
-    const userURL = new URL(`${this._url}`);
-    userURL.pathname = this._userURL;
-    userURL.searchParams.set('user', `${userID}`);
-
-    const user = {
-      "user": userID,
-      "display_name": faker.name.fullName(),
-      "image_original": faker.image.avatar(),
-    }
-
-    return user;
-  }
-}
-
-// Создание экземпляра класса
-const fakeAPI = new FakeSlackAPI(config)
-
-// Получение всех сообщений
-function getMessages(messagesURL) {
-  const parseURL = fakeAPI.setConversationRange(messagesURL); // Парсинг ссылки
-  const messagesFromApi = fakeAPI.getAllConversations(parseURL); // Получение основных сообщений
-
-  // Получение реплаев на все сообщения
-  const repliesArr = [];
-
-  messagesFromApi.forEach((item) => {
-    repliesArr.push(fakeAPI.getAllReplies(item.ts))
-  });
-
-  // Удаление повторяющихся сообщений
-  function filterByProperty(array, propertyName) {
-    const occurrences = {}
-
-    return array.filter((x) => {
-      const property = x[propertyName]
-      if (occurrences[property]) {
-        return false;
-      } else if
-      (occurrences[property] = true)
-        return true;
-    })
-  };
-
-  const allRepliesArr = [].concat(repliesArr[0], repliesArr[repliesArr.length - 1]);
-
-  const repliesMessages = []
-
-  allRepliesArr.forEach((item) => {
-    repliesMessages.push({
-      'text': item.text,
-      'ts': item.ts,
-      'user': item.user,
-      'isReply': true
-    })
-  })
-
-  const allMessages = [].concat(messagesFromApi, repliesMessages);
-  const filteredMessages = filterByProperty(allMessages, 'text');
-
-  return filteredMessages;
 };
 
-function getUserInfo(messageArr) {
-  const users = [];
+const runApp = async () => {
+  const slackAPI = new FakeSlackAPI(config);
+  const usersMap = new Map([]);
 
-  messageArr.forEach((item) => {
-    if (users.find(user => user.user === item.user)) {
-return
-    } else {
-      users.push(fakeAPI.getAllUsers(item.user))
-    }
+  const isSetConversation = slackAPI.setConversationRange(objURL);
+  if (!isSetConversation) {
+    throw new Error('Ошибка установки tConversationRange');
+  }
+
+  const messages = await slackAPI.getAllConversations();
+  const repliesPromises = messages
+    .filter(({ reply_count }) => reply_count > 0)
+    .map(({ thread_ts }) => slackAPI.getAllReplies(thread_ts));
+
+  const replies = await Promise.all(repliesPromises)
+    .then((replies) => replies.flat().reverse());
+
+  messages.forEach((message) => {
+    const { user } = message;
+    usersMap.set(user, null);
+  });
+  replies.forEach((message) => {
+    const { user } = message;
+    usersMap.set(user, null);
   });
 
-  return users;
-};
+  const usersPromise = Array
+    .from(usersMap.keys())
+    .map((userID) => slackAPI.getUser(userID)
+      .then((user) => {
+        usersMap.set(userID, user);
+      }));
+  await Promise.all(usersPromise);
 
-function prepaireForRender(messages, users) {
-  const messagesForRender = [];
-
-  messages.forEach((item) => {
-    messagesForRender.push({
-      'text': item.text,
-      'ts': item.ts,
-      'user': users.find(user => user.user === item.user).display_name,
-      'avatar': users.find(user => user.user === item.user).image_original,
-      'isReply': item.isReply
-    })
-  });
-
-  return messagesForRender;
-}
-
-const allMessages = getMessages(objURL);
-const users = getUserInfo(allMessages);
-const messagesForRender = prepaireForRender(allMessages, users);
-
-const runApp = (messagesData) => {
-  const container = document.querySelector('.card__list');
-
-  const generateCard = (dataCards) => {
-    const cardElementMain = document
+  const generateCard = (message, usersDataMap) => {
+    const cardElement = document
       .querySelector('#card-main')
       .content
       .querySelector('.card')
       .cloneNode(true);
 
-    const cardTitle = cardElementMain.querySelector('.card__title');
-    const cardText = cardElementMain.querySelector('.card__text');
-    const cardAvatar = cardElementMain.querySelector('.card__avatar');
+    const cardTitle = cardElement.querySelector('.card__title');
+    const cardText = cardElement.querySelector('.card__text');
+    const cardAvatar = cardElement.querySelector('.card__avatar');
 
-    cardTitle.textContent = dataCards.user;
-    cardText.textContent = dataCards.text;
-    cardAvatar.src = dataCards.avatar;
+    cardTitle.textContent = usersDataMap.get(message.user).display_name;
+    cardText.textContent = message.text;
+    cardAvatar.src = usersDataMap.get(message.user).image_original;
 
-    if (dataCards.isReply) {
-    // вставка реплая
-    }
-
-    return cardElementMain;
+    return cardElement;
   };
 
-  const renderItems = (messagesData) => {
+  messages.forEach((message) => {
+    const container = document.querySelector('.card__list');
 
-    messagesData.forEach((card) => {
-      container.prepend(generateCard(card))
-    })
-  }
+    if (message.reply_count === 0) {
+      container.append(generateCard(message, usersMap));
+    } else {
+      container.append(generateCard(message, usersMap));
 
-  renderItems(messagesData)
+      const repliesUl = document.createElement('ul');
+
+      replies.forEach((replie) => {
+        if (replie.thread_ts === message.thread_ts && replie.text !== message.text) {
+          repliesUl.prepend(generateCard(replie, usersMap));
+          container.append(repliesUl);
+        }
+      });
+    }
+  });
 };
 
-runApp(messagesForRender);
+runApp();
